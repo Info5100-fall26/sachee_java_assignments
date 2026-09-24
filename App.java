2fee8b607917614005f67344a0522d6bc9a16073
nenesachee <36198425+nenesachee@users.noreply.github.com>
2026-09-24 12:44:49 -0700

    Add files via upload

---
+++ App.java
@@ -0,0 +1,18 @@
+package edu.neu.mgen;
+
+/**
+ * Hello world! 
+ * 
+ */
+
+/* 
+    This is my first Java Assignment
+*/
+public class App
+{
+    public static void main( String[] args )
+    {
+        //print the below statement
+        System.out.println( "It is my first Java program" );
+    }
+}
